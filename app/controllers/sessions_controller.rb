cbd8c4ee46deae78f9d230b2b7e06aefe224eea0
Artem Baguinski <abaguinski@depraktijkindex.nl>
2016-12-29 15:47:34 +0100

    oauth callback route and it's controller#action

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def create
+    account = Account.find_or_create_from_auth_hash(auth_hash)
+    session[:account_id] = account.id
+    flash[:success] = "You're in"
+    redirect_to root_path
+  end
+
+  protected
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/oauth2callback" => "sessions#create"
 end
