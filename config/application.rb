e6e7e87a9a110e15682016ee396372c73fc966ea
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-01-01 01:33:15 +0100

    add babelify and es6 preprocessing

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,11 @@ module Anno
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
+
+    # Configure Browserify to use babelify to compile ES6
+    config.browserify_rails.commandline_options = "-t [ babelify --presets [ es2015 ] ]"
+
+    # Run on all javascript files
+    config.browserify_rails.force = true
   end
 end
