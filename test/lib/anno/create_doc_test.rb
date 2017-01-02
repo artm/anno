1f701de5cd3ed99e7b48ed55edf6cf8df4d7227c
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-01-02 21:41:22 +0100

    start testing the CreateDoc with Libellus Islandorum

--- test/lib/anno/create_doc_test.rb
+++ test/lib/anno/create_doc_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CreateDocTest < ActiveSupport::TestCase
+  Libellus = File.read("test/data/libellus_islandorum.txt").freeze
+  setup do
+    @creator = Anno::CreateDoc.new(
+      title: "Libellus Islandorum",
+      language: "Old Norse",
+      text: Libellus
+    )
+    @parsed_text = @creator.options[:text]
+  end
+
   test "parses paragraphs" do
-    assert false
+    assert @parsed_text.is_a? Array
+    assert @parsed_text.size == 6
+  end
+
+  test "parses sentences" do
+    expected_sizes = [4, 1, 2, 4, 2, 6]
+    @parsed_text.zip(expected_sizes).each do |paragraph, expected_size|
+      assert paragraph.is_a? Array
+      assert paragraph.size == expected_size
+    end
   end
 end
