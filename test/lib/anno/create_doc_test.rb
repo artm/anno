f1b76be6f89a7903990f6afe227956f3c86652b3
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-01-03 09:28:36 +0100

    more readable tests

--- test/lib/anno/create_doc_test.rb
+++ test/lib/anno/create_doc_test.rb
@@ -3,22 +3,22 @@ require 'test_helper'
 class CreateDocTest < ActiveSupport::TestCase
   setup do
     text = file_fixture("libellus_islandorum.txt").read.freeze
-    @creator = Anno::CreateDoc.new(
+    creator = Anno::CreateDoc.new(
       title: "Libellus Islandorum",
       language: "Old Norse",
       text: text
     )
-    @parsed_text = @creator.options[:text]
+    @paragraphs = creator.options[:text]
   end
 
-  test "parses paragraphs" do
-    assert_kind_of Array, @parsed_text
-    assert_equal @parsed_text.size, 6
+  test "splits text into paragraphs" do
+    assert_kind_of Array, @paragraphs
+    assert_equal @paragraphs.size, 6
   end
 
-  test "parses sentences" do
+  test "splits paragraphs into sentences" do
     expected_sizes = [4, 1, 2, 4, 2, 6]
-    @parsed_text.zip(expected_sizes).each do |paragraph, expected_size|
+    @paragraphs.zip(expected_sizes).each do |paragraph, expected_size|
       assert_kind_of Array, paragraph
       assert_equal paragraph.size, expected_size
     end
