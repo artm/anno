b4a5435a5aaac5f7bc8c2b2e813e5a812369ae8a
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-01-06 21:37:50 +0100

    added the inputs in the annotation editor

--- app/assets/javascripts/sentence-annotator.js
+++ app/assets/javascripts/sentence-annotator.js
@@ -1,20 +1,44 @@
 import React from "react";
 
+class PartOfSpeech extends React.Component {
+  render() {
+    return <input type="text" placeholder="part of speech" />;
+  }
+}
+
+class DictionaryForm extends React.Component {
+  render() {
+    return <input type="text" placeholder="dictionary form" />;
+  }
+}
+
+class HereForm extends React.Component {
+  render() {
+    return <input type="text" placeholder="here form" />;
+  }
+}
+
+class HereMeaning extends React.Component {
+  render() {
+    return <input type="text" placeholder="here meaning" />;
+  }
+}
+
 class Word extends React.Component {
   render() {
     return <div className="word-annotator row">
       <div className="here-word double-row medium-2 columns">{this.props.word}</div>
       <div className="medium-8 columns">
         <div className="row">
-          <div className="part-of-speech medium-4 columns">part of speech</div>
-          <div className="dictionary-form dict medium-4 columns">dictionary form</div>
-          <div className="here-form medium-4 columns">here form</div>
+          <div className="part-of-speech medium-3 columns"><PartOfSpeech /></div>
+          <div className="dictionary-form dict medium-4 columns"><DictionaryForm /></div>
+          <div className="here-form medium-5 columns"><HereForm/></div>
         </div>
         <div className="row">
           <div className="dictionary-meanings dict medium-12 columns">dictionary meaning</div>
         </div>
       </div>
-      <div className="here-meaning double-row medium-2 columns">here meaning</div>
+      <div className="here-meaning double-row medium-2 columns"><HereMeaning/></div>
     </div>;
   }
 }
