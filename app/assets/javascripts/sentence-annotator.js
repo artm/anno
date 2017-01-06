e0cd11bd032c1dccce89138fb23b03a0a5e75b67
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-01-06 15:29:59 +0100

    initial layout of the annotation editor

--- app/assets/javascripts/sentence-annotator.js
+++ app/assets/javascripts/sentence-annotator.js
@@ -2,8 +2,19 @@ import React from "react";
 
 class Word extends React.Component {
   render() {
-    return <div className="word-annotator">
-      {this.props.word}
+    return <div className="word-annotator row">
+      <div className="here-word double-row large-2 columns">{this.props.word}</div>
+      <div className="large-8 columns">
+        <div className="row">
+          <div className="part-of-speech large-4 columns">part of speech</div>
+          <div className="dictionary-form dict large-4 columns">dictionary form</div>
+          <div className="here-form large-4 columns">here form</div>
+        </div>
+        <div className="row">
+          <div className="dictionary-meanings dict large-12 columns">dictionary meaning</div>
+        </div>
+      </div>
+      <div className="here-meaning double-row large-2 columns">here meaning</div>
     </div>;
   }
 }
