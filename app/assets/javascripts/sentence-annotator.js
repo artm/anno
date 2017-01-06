9e710922ca8e0bc6bf52466454e6f7febe1b50a2
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-01-06 11:33:38 +0100

    skeleton for a word annotator

--- app/assets/javascripts/sentence-annotator.js
+++ app/assets/javascripts/sentence-annotator.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+class Word extends React.Component {
+  render() {
+    return <div className="word-annotator">
+      {this.props.word}
+    </div>;
+  }
+}
+
 export default class SentenceAnnotator extends React.Component {
   render() {
-    return <div>{this.props.sentence.words.map((w,i) => <div key={i}>{w.word}</div>)}</div>
+    return <div className="sentence-annotator">{this.props.sentence.words.map((w,i) => <Word key={i} word={w.word} />)}</div>
   }
 }
