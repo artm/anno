58978b71030929f4c7f1521bd73ac4f3ec8c4d0e
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-01-06 15:56:03 +0100

    more styling

--- app/assets/javascripts/sentence-annotator.js
+++ app/assets/javascripts/sentence-annotator.js
@@ -3,18 +3,18 @@ import React from "react";
 class Word extends React.Component {
   render() {
     return <div className="word-annotator row">
-      <div className="here-word double-row large-2 columns">{this.props.word}</div>
-      <div className="large-8 columns">
+      <div className="here-word double-row medium-2 columns">{this.props.word}</div>
+      <div className="medium-8 columns">
         <div className="row">
-          <div className="part-of-speech large-4 columns">part of speech</div>
-          <div className="dictionary-form dict large-4 columns">dictionary form</div>
-          <div className="here-form large-4 columns">here form</div>
+          <div className="part-of-speech medium-4 columns">part of speech</div>
+          <div className="dictionary-form dict medium-4 columns">dictionary form</div>
+          <div className="here-form medium-4 columns">here form</div>
         </div>
         <div className="row">
-          <div className="dictionary-meanings dict large-12 columns">dictionary meaning</div>
+          <div className="dictionary-meanings dict medium-12 columns">dictionary meaning</div>
         </div>
       </div>
-      <div className="here-meaning double-row large-2 columns">here meaning</div>
+      <div className="here-meaning double-row medium-2 columns">here meaning</div>
     </div>;
   }
 }
