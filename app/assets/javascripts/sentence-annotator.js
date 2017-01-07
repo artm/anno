d815586f19d1c63031495577b666e5fc8273f8e0
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-01-07 15:06:55 +0100

    reusable anno input and reuse it

--- app/assets/javascripts/sentence-annotator.js
+++ app/assets/javascripts/sentence-annotator.js
@@ -1,36 +1,59 @@
 import React from "react";
 
-class PartOfSpeech extends React.Component {
+class AnnoInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: props.anno.part_of_speech || ""};
     this.anno = props.anno;
+    this.state = {value: this.anno[this.annoKey()] || ""};
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
     this.setState({value: event.target.value});
-    this.anno.part_of_speech = event.target.value;
+    this.anno[this.annoKey()] = event.target.value;
   }
   render() {
-    return <input type="text" placeholder="part of speech" value={this.state.value} onChange={this.handleChange}/>;
-  }
-}
-
-class DictionaryForm extends React.Component {
-  render() {
-    return <input type="text" placeholder="dictionary form" />;
-  }
-}
-
-class HereForm extends React.Component {
-  render() {
-    return <input type="text" placeholder="here form" />;
+    return <input
+      type="text"
+      placeholder={this.placeholder()}
+      value={this.state.value}
+      onChange={this.handleChange}
+    />;
   }
 }
 
-class HereMeaning extends React.Component {
-  render() {
-    return <input type="text" placeholder="here meaning" />;
+class PartOfSpeech extends AnnoInput {
+  placeholder() {
+    return "part of speech";
+  }
+  annoKey() {
+    return "part_of_speech";
+  }
+}
+
+class DictionaryForm extends AnnoInput {
+  placeholder() {
+    return "dictionary form";
+  }
+  annoKey() {
+    return "dictionary_form";
+  }
+}
+
+class HereForm extends AnnoInput {
+  placeholder() {
+    return "here form";
+  }
+  annoKey() {
+    return "here_form";
+  }
+}
+
+class HereMeaning extends AnnoInput {
+  placeholder() {
+    return "here meaning";
+  }
+  annoKey() {
+    return "here_meaning";
   }
 }
 
@@ -41,19 +64,21 @@ class Word extends React.Component {
   }
 
   render() {
+    var anno = this.props.word.anno;
+
     return <div className="word-annotator row">
       <div className="here-word double-row medium-2 columns">{this.props.word.word}</div>
       <div className="medium-8 columns">
         <div className="row">
-          <div className="part-of-speech medium-3 columns"><PartOfSpeech anno={this.props.word.anno}/></div>
-          <div className="dictionary-form dict medium-4 columns"><DictionaryForm /></div>
-          <div className="here-form medium-5 columns"><HereForm/></div>
+          <div className="part-of-speech medium-3 columns"><PartOfSpeech anno={anno}/></div>
+          <div className="dictionary-form dict medium-4 columns"><DictionaryForm anno={anno}/></div>
+          <div className="here-form medium-5 columns"><HereForm anno={anno}/></div>
         </div>
         <div className="row">
           <div className="dictionary-meanings dict medium-12 columns">dictionary meaning</div>
         </div>
       </div>
-      <div className="here-meaning double-row medium-2 columns"><HereMeaning/></div>
+      <div className="here-meaning double-row medium-2 columns"><HereMeaning anno={anno}/></div>
     </div>;
   }
 }
