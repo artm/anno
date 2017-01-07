1116521151998d03b66ae8e0118049ff5de8105d
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-01-07 16:12:00 +0100

    AutoSave timeouts

--- app/assets/javascripts/auto-save.js
+++ app/assets/javascripts/auto-save.js
@@ -1,6 +1,12 @@
 export default class AutoSave {
+  constructor() {
+    console.log("new AutoSave");
+    this.delay = 5000;
+    this.initDiff();
+  }
+
   static singleton() {
-    if (!this.instance_) {
+    if (!this.instance) {
       this.instance = new AutoSave();
     }
     return this.instance;
@@ -10,7 +16,32 @@ export default class AutoSave {
     this.singleton().updateWord(wordKey, update);
   }
 
+  initDiff() {
+    this.diff = {
+      words: {}
+    };
+  }
+
   updateWord(wordKey, update) {
-    console.log("updateWord", wordKey, update);
+    if (!this.diff.words[wordKey]) {
+      this.diff.words[wordKey] = {};
+    }
+    $.extend(true, this.diff.words[wordKey], update)
+    this.postponeSave();
+  }
+
+  postponeSave() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+    this.timeoutId = setTimeout(() => {
+      this.timeoutId = null;
+      this.save();
+    }, this.delay);
+  }
+
+  save() {
+    console.log("save", this.diff);
+    this.initDiff();
   }
 }
