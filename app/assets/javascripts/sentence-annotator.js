53f4d81095464b79d61c3b79785596657a3de933
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-01-12 09:11:27 +0100

    less subclassing, upper state still broken

--- app/assets/javascripts/sentence-annotator.js
+++ app/assets/javascripts/sentence-annotator.js
@@ -7,30 +7,21 @@ import SuggestionSource from "suggestion-source";
 class AnnoInput extends React.Component {
   constructor(props) {
     super(props);
-    this.anno = props.word.anno;
-    this.state = {value: this.anno[this.annoKey()] || ""};
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(value) {
-    this.setState({value: value});
-    if (this.props.onChange) {
-      this.props.onChange(value);
-    }
   }
   render() {
     let source = this.props.suggestionSource;
     let context = this.props.word;
     return (
         <Autocomplete
-          value={this.state.value}
-          inputProps={{placeholder: this.placeholder(), type: "text"}}
+          value={this.props.value}
+          inputProps={{placeholder: this.props.placeholder, type: "text"}}
           items={source.suggestions(context)}
           getItemValue={(item) => item}
           shouldItemRender={(suggestion, currentInput) =>
             source.shouldRenderSuggestion(suggestion, currentInput, context)}
           sortItems={(a,b,currentInput) => source.compareSuggestions(a,b,currentInput, context)}
-          onChange={(event, value) => this.handleChange(value)}
-          onSelect={value => this.handleChange(value)}
+          onChange={(event, value) => this.props.onChange(value)}
+          onSelect={value => this.props.onChange(value)}
           renderItem={(item, isHighlighted) => (
             <div
               style={isHighlighted ? {background: "#ffff99"} : {}}
@@ -42,46 +33,11 @@ class AnnoInput extends React.Component {
   }
 }
 AnnoInput.propTypes = {
+  value: React.PropTypes.string.isRequired,
   word: React.PropTypes.object.isRequired,
-  wordKey: React.PropTypes.string.isRequired,
+  placeholder: React.PropTypes.string,
   suggestionSource: React.PropTypes.object.isRequired,
-  onChange: React.PropTypes.func,
-}
-
-class PartOfSpeech extends AnnoInput {
-  placeholder() {
-    return "part of speech";
-  }
-  annoKey() {
-    return "part_of_speech";
-  }
-}
-
-class DictionaryForm extends AnnoInput {
-  placeholder() {
-    return "dictionary form";
-  }
-  annoKey() {
-    return "dictionary_form";
-  }
-}
-
-class HereForm extends AnnoInput {
-  placeholder() {
-    return "here form";
-  }
-  annoKey() {
-    return "here_form";
-  }
-}
-
-class HereMeaning extends AnnoInput {
-  placeholder() {
-    return "here meaning";
-  }
-  annoKey() {
-    return "here_meaning";
-  }
+  onChange: React.PropTypes.func.isRequired,
 }
 
 class Word extends React.Component {
@@ -99,32 +55,35 @@ class Word extends React.Component {
   }
   render() {
     var word = this.state.word;
-    var wordKey = this.props.wordKey;
+    var anno = word.anno;
 
     return <div className="word-annotator row">
       <div className="here-word double-row medium-3 columns">{this.props.word.word}</div>
       <div className="medium-6 columns">
         <div className="row">
           <div className="part-of-speech medium-3 columns">
-            <PartOfSpeech
+            <AnnoInput
+              value={anno.part_of_speech || ""}
+              placeholder="part of speech"
               word={word}
-              wordKey={wordKey}
               suggestionSource={docText.suggestions.partOfSpeech}
               onChange={(value) => {this.updateAnno("part_of_speech", value)}}
             />
           </div>
           <div className="dictionary-form dict medium-4 columns">
-            <DictionaryForm
+            <AnnoInput
+              value={anno.dictionary_form || ""}
+              placeholder="dictionary form"
               word={word}
-              wordKey={wordKey}
               suggestionSource={docText.suggestions.dictionaryForm}
               onChange={(value) => {this.updateAnno("dictionary_form", value)}}
             />
           </div>
           <div className="here-form medium-5 columns">
-            <HereForm
+            <AnnoInput
+              value={anno.here_form || ""}
+              placeholder="here form"
               word={word}
-              wordKey={wordKey}
               suggestionSource={docText.suggestions.hereForm}
               onChange={(value) => {this.updateAnno("here_form", value)}}
             />
@@ -135,9 +94,10 @@ class Word extends React.Component {
         </div>
       </div>
       <div className="here-meaning double-row medium-3 columns">
-        <HereMeaning
+        <AnnoInput
+          value={anno.here_meaning || ""}
+          placeholder="here meaning"
           word={word}
-          wordKey={wordKey}
           suggestionSource={docText.suggestions.hereMeaning}
           onChange={(value) => {this.updateAnno("here_meaning", value)}}
         />
